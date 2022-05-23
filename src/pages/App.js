45678cf565a51591e8281acfc5e6183e05e06d59
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2022-05-23 12:08:13 +0200

    terms of service

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Faq from "../pages/faq";
+import TermsOfService from "../pages/terms-of-service";
 import Navbar from "../components/navbar";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/faq" element={<Faq />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
       </Routes>
     </BrowserRouter>
   );
